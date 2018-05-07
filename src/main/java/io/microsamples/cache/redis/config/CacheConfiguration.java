0e9fc63b2592dead26aa8e5728fd18a709bb8512
Alex Shumilov <ashumilov@pivotal.io>
2018-05-07 08:29:26 -0400

    Move redis configurations to autoconfiguratio project.

--- src/main/java/io/microsamples/cache/redis/config/CacheConfiguration.java
+++ src/main/java/io/microsamples/cache/redis/config/CacheConfiguration.java
@@ -1,60 +1,17 @@
 package io.microsamples.cache.redis.config;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cloud.Cloud;
 import org.springframework.cloud.CloudFactory;
 import org.springframework.cloud.service.common.RedisServiceInfo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableCaching
-public class CacheConfiguration extends CachingConfigurerSupport {
-
-    @Bean
-    public KeyGenerator keyGenerator() {
-        return new KeyGenerator() {
-            @Override
-            public Object generate(Object target, Method method, Object... params) {
-                List<String> methodParams = new ArrayList<>();
-                for (Object param : params) {
-                    methodParams.add(param.toString());
-                }
-
-                CacheKey key = CacheKey.builder()
-                        .className(target.getClass().toString())
-                        .method(method.toString())
-                        .params(methodParams)
-                        .build();
-
-                ObjectMapper objectMapper = new ObjectMapper();
-
-                try {
-                    return objectMapper.writeValueAsString(key);
-                } catch (JsonProcessingException e) {
-                    throw new RuntimeException("Not able to process key for cache", e);
-                }
-
-            }
-        };
-    }
-
+public class CacheConfiguration{
     @Bean
     @Profile("cloud")
     public RedisConnectionFactory redisConnectionFactory() {
@@ -64,36 +21,4 @@ public class CacheConfiguration extends CachingConfigurerSupport {
         String serviceID = serviceInfo.getId();
         return cloud.getServiceConnector(serviceID, RedisConnectionFactory.class, null);
     }
-
-
-    @SuppressWarnings("rawtypes")
-    @Bean
-    public CacheManager cacheManager(RedisTemplate redisTemplate) {
-        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
-        rcm.setDefaultExpiration(600); //<-- expire cache every 10 minutes
-        return rcm;
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate();
-        template.setConnectionFactory(redisConnectionFactory);
-
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.afterPropertiesSet();
-        return template;
-    }
-
-    @Bean
-    public RedisTemplate<String, String> keyRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        StringRedisTemplate template = new StringRedisTemplate();
-        template.setConnectionFactory(redisConnectionFactory);
-
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        template.afterPropertiesSet();
-        return template;
-    }
-
 }
